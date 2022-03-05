e0328e4ae5049753bbf4bd4655a211ae57a5d46e
LordRonz <aarontanhar2000@gmail.com>
2022-03-05 14:02:30 +0700

    feat: initialize pages and libs :rocket:

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,9 +9,20 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='en'>
-        <Head />
-        <body>
+      <Html lang='en' className='scroll-smooth'>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='anonymous'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body className='bg-light text-dark  dark:bg-dark dark:text-light'>
           <Main />
           <NextScript />
         </body>
