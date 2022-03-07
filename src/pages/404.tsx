e54412b0217c5f89af843cfc8249f0ec88c15b03
LordRonz <aarontanhar2000@gmail.com>
2022-03-07 19:17:50 +0700

    feat: add quote to 404 page :rocket:

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 
+import Accent from '@/components/Accent';
 import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
 
@@ -10,8 +11,13 @@ const NotFound: NextPage = () => (
       <section className='bg-black text-primary-50'>
         <div className='layout flex min-h-screen flex-col items-center justify-center gap-y-40 text-center'>
           <div className='flex flex-col gap-y-4'>
-            <h1 className='text-8xl text-primary-300'>404</h1>
+            <h1 className='text-8xl'>
+              <Accent className='from-primary-300 via-primary-100/70 to-primary-400'>
+                404
+              </Accent>
+            </h1>
             <h2>Page Not Found</h2>
+            <p>Kamu kesasar, aku kangen</p>
           </div>
 
           <p className='text-xl text-primary-200'>
