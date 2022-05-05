66a69e0e4f39c92ed2b114718c28ee879cb31837
LordRonz <aarontanhar2000@gmail.com>
2022-05-05 16:34:27 +0700

    feat: customize styledinput :rocket:

--- src/components/forms/StyledInput.tsx
+++ src/components/forms/StyledInput.tsx
@@ -2,15 +2,21 @@ import * as React from 'react';
 
 import clsxm from '@/lib/clsxm';
 
+type StyledInputProps = {
+  underline?: boolean;
+} & React.ComponentPropsWithoutRef<'input'>;
+
 const StyledInput = ({
   className,
+  underline = false,
   ...rest
-}: React.ComponentPropsWithoutRef<'input'>) => {
+}: StyledInputProps) => {
   return (
     <input
       className={clsxm(
         'w-full rounded-md dark:bg-dark',
-        'border border-gray-300 dark:border-gray-500',
+        underline ? 'border-0 border-b-2' : 'border',
+        'border-gray-300 dark:border-gray-500',
         'transition-all duration-200 focus:border-primary-300 focus:outline-none focus:ring-0 dark:focus:border-primary-300',
         className
       )}
