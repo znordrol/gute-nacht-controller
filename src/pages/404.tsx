34363227d2bb6ddd49c606a2b3cf041185053179
LordRonz <aarontanhar2000@gmail.com>
2022-05-06 13:10:23 +0700

    feat: better accent coloring :rocket:

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,9 +13,7 @@ const NotFound: NextPage = () => (
         <div className='layout flex min-h-screen flex-col items-center justify-center gap-y-40 text-center'>
           <div className='flex flex-col gap-y-4'>
             <h1 className='text-8xl'>
-              <Accent className='from-primary-300 via-primary-100/70 to-primary-400'>
-                404
-              </Accent>
+              <Accent className=''>404</Accent>
             </h1>
             <h2>Page Not Found</h2>
             <p>Kamu kesasar, aku kangen 🥺</p>
