56c693368fabeae447981616d7d52ead22eb826d
LordRonz <aarontanhar2000@gmail.com>
2022-05-26 00:47:09 +0700

    feat: counter hide 0 count :rocket:

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -45,48 +45,75 @@ const Counter = ({
     setTimeLeft(calculateTimeLeft(endDate));
   }, [endDate]);
 
+  if (isADayAfter(endDate)) {
+    return (
+      <div
+        className={clsxm(
+          'grid grid-cols-5 divide-x divide-neutral-400',
+          className
+        )}
+      >
+        <div
+          className={clsxm(
+            'flex flex-col items-center justify-center px-4',
+            colWrapperClassName
+          )}
+        >
+          <h2 className={numberClassName}>0</h2>
+          <p>Bulan</p>
+        </div>
+        <div>
+          <h2 className={numberClassName}>0</h2>
+          <p>Hari</p>
+        </div>
+        <div>
+          <h2 className={numberClassName}>0</h2>
+          <p>Jam</p>
+        </div>
+        <div>
+          <h2 className={numberClassName}>0</h2>
+          <p>Menit</p>
+        </div>
+        <div>
+          <h2 className={numberClassName}>0</h2>
+          <p>Detik</p>
+        </div>
+      </div>
+    );
+  }
+
+  const timeLefts = [
+    ...(timeLeft?.months ? [{ t: timeLeft.months, label: 'Bulan' }] : []),
+    ...(timeLeft?.days ? [{ t: timeLeft.days, label: 'Hari' }] : []),
+    ...(timeLeft?.hours ? [{ t: timeLeft.hours, label: 'Jam' }] : []),
+    ...(timeLeft?.minutes ? [{ t: timeLeft.minutes, label: 'Menit' }] : []),
+    ...(timeLeft?.seconds ? [{ t: timeLeft.seconds, label: 'Detik' }] : []),
+  ];
+
   return (
     <div
       className={clsxm(
-        'grid grid-cols-5 divide-x divide-neutral-400',
+        'grid divide-x divide-neutral-400',
+        timeLefts.length === 1 && 'grid-cols-1',
+        timeLefts.length === 2 && 'grid-cols-2',
+        timeLefts.length === 3 && 'grid-cols-3',
+        timeLefts.length === 4 && 'grid-cols-4',
+        timeLefts.length === 5 && 'grid-cols-5',
         className
       )}
     >
-      <div
-        className={clsxm(
-          'flex flex-col items-center justify-center px-4',
-          colWrapperClassName
-        )}
-      >
-        <h2 className={numberClassName}>
-          {isADayAfter(endDate) ? 0 : timeLeft?.months}
-        </h2>
-        <p>Bulan</p>
-      </div>
-      <div>
-        <h2 className={numberClassName}>
-          {isADayAfter(endDate) ? 0 : timeLeft?.days}
-        </h2>
-        <p>Hari</p>
-      </div>
-      <div>
-        <h2 className={numberClassName}>
-          {isADayAfter(endDate) ? 0 : timeLeft?.hours}
-        </h2>
-        <p>Jam</p>
-      </div>
-      <div>
-        <h2 className={numberClassName}>
-          {isADayAfter(endDate) ? 0 : timeLeft?.minutes}
-        </h2>
-        <p>Menit</p>
-      </div>
-      <div>
-        <h2 className={numberClassName}>
-          {isADayAfter(endDate) ? 0 : timeLeft?.seconds}
-        </h2>
-        <p>Detik</p>
-      </div>
+      {timeLefts.map(({ t, label }) => (
+        <div
+          className={clsxm(
+            'flex flex-col items-center justify-center px-4',
+            colWrapperClassName
+          )}
+          key={label}
+        >
+          <h2 className={numberClassName}>{t}</h2>
+          <p>{label}</p>
+        </div>
+      ))}
     </div>
   );
 };
